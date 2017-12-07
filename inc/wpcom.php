5110e7512bc139b00ca4cd2848500880776a9490
Kory Northrop <korynorthrop@Lagniappe.local>
2017-12-07 13:04:11 -0800

    Add firestarter-demo Underscores files

---
+++ inc/wpcom.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * WordPress.com-specific functions and definitions
+ *
+ * This file is centrally included from `wp-content/mu-plugins/wpcom-theme-compat.php`.
+ *
+ * @package Firestarter_Demo
+ */
+
+/**
+ * Adds support for wp.com-specific theme functions.
+ *
+ * @global array $themecolors
+ */
+function firestarter_demo_wpcom_setup() {
+	global $themecolors;
+
+	// Set theme colors for third party services.
+	if ( ! isset( $themecolors ) ) {
+		$themecolors = array(
+			'bg'     => '',
+			'border' => '',
+			'text'   => '',
+			'link'   => '',
+			'url'    => '',
+		);
+	}
+}
+add_action( 'after_setup_theme', 'firestarter_demo_wpcom_setup' );
